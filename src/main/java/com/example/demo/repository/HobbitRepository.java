0dd0f91c6858402e85b0049991aae0592d702084
uki <wyspianski.lukasz@gmail.com>
2021-12-13 11:38:13 +0100

    Supermoce: repozytorium

--- src/main/java/com/example/demo/repository/HobbitRepository.java
+++ src/main/java/com/example/demo/repository/HobbitRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.repository;
 
-public class HobbitRepository {
+import com.example.demo.model.Hobbit;
+import org.springframework.data.jpa.repository.JpaRepository;
+/*
+    Używamy bardziej elastycznej opcji wtedy, gdy to niezbędne
+    - rozszerzamy istniejący interfejs
+    - dodajemy własne metody (nazwa metody nie jest trywialna!)
+    - implementujemy własne repozytorium i 'miksujemy je z interfejsem'
+    
+ */
+public interface HobbitRepository extends JpaRepository<Hobbit,Long> {
 }
