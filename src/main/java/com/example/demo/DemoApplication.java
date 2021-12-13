c5869bf3b122d649bd06bb4b9b7b7107f731e6f7
uki <wyspianski.lukasz@gmail.com>
2021-12-13 11:00:31 +0100

    Kontener Spring - lista ziaren

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		Arrays.stream(ctx.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
