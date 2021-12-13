4ada2f50efd23f7f8f376edf3393392167e4975d
uki <wyspianski.lukasz@gmail.com>
2021-12-13 11:16:33 +0100

    Supermoce: encja i lombok

--- src/main/java/com/example/demo/model/Hobbit.java
+++ src/main/java/com/example/demo/model/Hobbit.java
@@ -1,4 +1,21 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Hobbit {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private String lastName;
 }
