0e3cb21c3a4e26902504f0d91c1192ee2f74a53b
PWLODARC <p.wlodarczyk.sec@gmail.com>
2021-12-13 14:52:49 +0100

    Nowy start projektu, cwiczenie

---
+++ noweDemo/src/test/java/com/example/nowedemo/NoweDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nowedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoweDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
