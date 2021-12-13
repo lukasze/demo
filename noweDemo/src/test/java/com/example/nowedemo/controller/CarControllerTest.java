1e46a4e9b94eb42af769811076851098b887aa2c
really-need-an-api-key <330983-really-need-an-api-key@users.noreply.gitlab.com>
2021-12-13 15:49:21 +0100

    Dodanie testów na 404, 405. Dodanie testu sprawdzającego liczność.

--- noweDemo/src/test/java/com/example/nowedemo/controller/CarControllerTest.java
+++ noweDemo/src/test/java/com/example/nowedemo/controller/CarControllerTest.java
@@ -1,5 +1,9 @@
 package com.example.nowedemo.controller;
 
+import com.example.nowedemo.model.Car;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +11,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -20,6 +27,9 @@ class CarControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     @DisplayName("HTTP /cars -> 200 OK")
     void listCars() throws Exception {
@@ -27,5 +37,39 @@ class CarControllerTest {
                         MockMvcRequestBuilders.get("/cars").accept(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("HTTP /car -> 404 OK")
+    void listCar_returns404() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/car").accept(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("HTTP POST /cars -> 405 MNA")
+    void listCarsPOST_returns405() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/cars").accept(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+    @Test
+    @DisplayName("HTTP /cars -> 200 OK; 4 cars")
+    void listCars_returns4Cars() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                        MockMvcRequestBuilders.get("/cars").accept(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String json = mvcResult.getResponse().getContentAsString();
+
+        List<Car> cars = objectMapper.readValue(json, new TypeReference<>() {});
+
+        Assertions.assertEquals(4, cars.size());
     }
 }
