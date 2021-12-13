b0f6c497ade304f58b95d017da23abf313a65925
uki <wyspianski.lukasz@gmail.com>
2021-12-13 14:19:05 +0100

    Test - GET /hobbits -> HTTP 200, lista (2 elementy)

--- src/test/java/com/example/demo/controller/HobbitControllerTest.java
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Hobbit;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,42 +10,63 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-// Supermoc - Pełen kontekst Spring'a, użyty do testów
 @SpringBootTest
-// Supermoc - Spring wie jak skonfifgurować MockMvc
 @AutoConfigureMockMvc
 class HobbitControllerTest {
 
-    // API do testów endpointów, potrzebna konfiguracja dla Spring'a: @AutoConfigureMockMvc
     @Autowired
     private MockMvc mockMvc;
 
-    // Supermoc JUnit - zmiana w metodę testową
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     @DisplayName("HTTP GET /hobbits -> HTTP 200")
     void hobbitsShouldReturn200() throws Exception {
-        // given: zainicjalizowana baza danych (2 rekordy)
-
-        // when: wywołanie endpointu GET /hobbits
         mockMvc
-                // wywołanie endpointu
                 .perform(
                         MockMvcRequestBuilders
-                                // HTTP GET /hobbits
                                 .get("/hobbits")
-                                // Nagłówek żądania - JSON
                                 .accept(MediaType.APPLICATION_JSON)
                 )
-                // bardziej szczegółowe logowanie
                 .andDo(print())
-        // then: oczekujemy HTTP 200
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
+
+    @Test
+    @DisplayName("HTTP GET /hobbits -> HTTP 200; 2 hobbits")
+    void hobbitsShouldReturn200And2Hobbits() throws Exception {
+        /*
+            - wywołaj endpoint /hobbit
+            - sprawdź status HTTP
+            - utwórz z odpowiedzi obiekt MvcResult
+         */
+        var mvcResult = mockMvc
+                .perform(
+                        MockMvcRequestBuilders
+                                .get("/hobbits")
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        // wyciągnij z MvcResult JSON'a
+        String json = mvcResult.getResponse().getContentAsString();
+        // zmapuj JSONa do List<Hobbit>
+        List<Hobbit> hobbitsFromDb = objectMapper.readValue(json, new TypeReference<>() {});
+        // asercje na obiektach utworzonych z JSON'a
+        assertEquals(2, hobbitsFromDb.size());
+    }
+
 }
